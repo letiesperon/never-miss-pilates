cc117989a5a37af00547e0e8423cd057884fcb0e
Leti Esperon <esperonleticia@gmail.com>
2024-06-09 15:47:05 -0300

    bugsnag setup

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Bugsnag.configure do |config|
-  config.api_key = ENV.fetch('BUGSNAG_KEY', nil)
+  config.api_key = ENV.fetch('BUGSNAG_API_KEY', nil)
   config.app_version = ENV.fetch('HEROKU_RELEASE_VERSION', nil)
   config.release_stage = ENV['BUGSNAG_RELEASE_STAGE'] || ENV.fetch('RAILS_ENV', nil)
   config.enabled_release_stages = %w[production staging review]
