d6b5d646e8aabbded7164ffd1330ef91f37158f0
Leti Esperon <esperonleticia@gmail.com>
2025-01-28 22:12:57 -0300

    Fix timezone parsing

--- app/concepts/crc/class_finder.rb
+++ app/concepts/crc/class_finder.rb
@@ -37,25 +37,28 @@ module CRC
         class_name_matches = class_name.downcase.include?('cycle')
 
         class_start_date_raw = class_entry.fetch('startDate')
-        class_start_time = parse_hour(class_start_date_raw)
+        class_start_time = parse_time(class_start_date_raw)
         class_start_time_to_i = class_start_time.to_i
         datetime_to_i = datetime.to_i
         class_time_matches = class_start_time_to_i == datetime_to_i
 
         matches = class_name_matches && class_time_matches
 
-        unless matches
-          Bugsnag.leave_breadcrumb(
-            'Class not matched',
-            datetime:,
-            class_name_matches:,
-            class_time_matches:,
-            class_name:,
-            class_start_date_raw:,
-            class_start_time:,
-            class_start_time_to_i:,
-            datetime_to_i:
-          )
+        class_info = {
+          datetime:,
+          class_name_matches:,
+          class_time_matches:,
+          class_name:,
+          class_start_date_raw:,
+          class_start_time:,
+          class_start_time_to_i:,
+          datetime_to_i:
+        }
+
+        if matches
+          Bugsnag.leave_breadcrumb('Class matched', class_info)
+        else
+          Bugsnag.leave_breadcrumb('Class NOT matched', class_info)
         end
 
         matches
@@ -65,8 +68,12 @@ module CRC
       end
     end
 
-    def parse_hour(datetime)
-      Time.parse(datetime)
+    def parse_time(datetime_s)
+      # Has to assume the given datetime string is in Montevideo timezone,
+      # even if it lacks the timezone information.
+      # parse_time("2025-01-29T19:00:00") => Wed, 29 Jan 2025 19:00:00.000000000 -03 -03:00
+
+      ActiveSupport::TimeZone['America/Montevideo'].parse(datetime_s)
     end
 
     def validate_class_exists!
